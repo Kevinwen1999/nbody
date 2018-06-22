92d3b05b17abcc0cbf749f6983ba0f01b5ec766a
Kevinwen1999 <cybergodzilla1999@gmail.com>
2018-06-22 19:06:58 -0400

    Implement Collision Merge

--- quadtree.py
+++ quadtree.py
@@ -19,7 +19,9 @@ class Treenode:
         self.cx = x
         self.cy = y
         # Total mass
-        self.tm = 0,
+        self.tm = 0
+        # id of node stored
+        self.nid = 0
 
     # Insert a particle into the tree
     def insertNode(self, a):
@@ -28,6 +30,7 @@ class Treenode:
             self.cx = a.px * a.mass
             self.cy = a.py * a.mass
             self.tm = a.mass
+            self.nid = a.id
         else:
             q = getQuadrant(a, self)
             if self.child[q] is None:
@@ -38,6 +41,8 @@ class Treenode:
 
     # Calculate mass and center of mass
     def calcMass(self):
+        if self.n > 1:
+            self.tm = 0
         for j in range(4):
             if self.child[j] is None:
                 continue
@@ -54,8 +59,6 @@ class Treenode:
         r = distBetweenPoints(self.cx, self.cy, a.px, a.py)
         d = Width / self.w
         if r == 0 or d / r < Theta or self.n == 1:
-            tp = Particle()
-            tp.setP(self.cx, self.cy, self.tm)
             tax, tay = getAcceleration(self.cx, self.cy, self.tm, a)
             a.ax += tax
             a.ay += tay
@@ -64,3 +67,34 @@ class Treenode:
                 if self.child[j] is None:
                     continue
                 self.child[j].calcForce(a)
+
+    # Merge particles that are too close
+    def checkMerge(self, a, par):
+        if self.n == 1:
+            if self.nid == par[a].id or par[self.nid].exist == False:
+                return
+            # If collide, merge and conserve momentum
+            if distBetweenPoints(self.cx, self.cy, par[a].px, par[a].py) <= par[a].r + par[self.nid].r:
+                par[self.nid].exist = False
+                par[a].vx = (par[a].vx * par[a].mass + par[self.nid].vx * par[self.nid].mass) / (par[a].mass + par[self.nid].mass)
+                par[a].vy = (par[a].vy * par[a].mass + par[self.nid].vy * par[self.nid].mass) / (par[a].mass + par[self.nid].mass)
+                par[a].mass += par[self.nid].mass
+            return
+        for j in range(4):
+            if self.child[j] is None:
+                continue
+            x1, y1 = getTopCoordinate(self, j)
+            w, h = Width / self.w / 2.0, Height / self.w / 2.0
+            x2 = x1 + w
+            y2 = y1 + h
+            if checkCirinRec(par[a].px, par[a].py, par[a].r, x1, x2, y1, y2) or checkRecinCir(par[a].px, par[a].py, par[a].r, x1, x2, y1, y2):
+                self.child[j].checkMerge(a, par)
+            else:
+                for k in range(4):
+                    ax = x2 if 1 <= k <= 2 else x1
+                    ay = y2 if k >= 2 else y1
+                    bx = x2 if 1 <= (k + 1) % 4 <= 2 else x1
+                    by = y2 if (k + 1) % 4 >= 2 else y1
+                    if disttoLineSeg(ax, ay, bx, by, par[a].px, par[a].py) <= par[a].r:
+                        self.child[j].checkMerge(a, par)
+                        break
